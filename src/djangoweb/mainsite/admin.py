479bb1c02e46f58e6f54b5d7e4ace610fffdd870
jjasoncool <nameyearbirthday@gmail.com>
2021-01-12 01:54:22 +0800

    feat: admin page show data info

--- src/djangoweb/mainsite/admin.py
+++ src/djangoweb/mainsite/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import PostContent
 # Register your models here.
-admin.site.register(PostContent)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'remarks', 'pub_date')
+
+admin.site.register(PostContent, PostAdmin)
