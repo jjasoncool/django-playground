0b6049986f0118c23309fcab5eb2b4de1f35040e
jjasoncool <nameyearbirthday@gmail.com>
2021-01-16 13:56:04 +0800

    course 2 modify

--- src/djangoweb/mainsite/views.py
+++ src/djangoweb/mainsite/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import PostContent
 
 # Create your views here.
+def homepage(request):
+    posts = PostContent.objects.all()
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>")
+        post_lists.append("<small>" + str(post.body) + "</small><br>")
+    return HttpResponse(post_lists)
