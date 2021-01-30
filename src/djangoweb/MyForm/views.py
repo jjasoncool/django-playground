a57c4ce893e1a5e436b6f92447df8231dbbaf5ac
jjasoncool <nameyearbirthday@gmail.com>
2021-01-30 14:04:08 +0800

    course MyForm add get parameters

--- src/djangoweb/MyForm/views.py
+++ src/djangoweb/MyForm/views.py
@@ -4,5 +4,21 @@ from django.shortcuts import render
 from MyForm import models
 from django.template.loader import get_template
 
+
 def form_index(request):
+    # 定義選單內容
+    years = range(1960,2021)
+    try:
+        urid = request.GET['user_id']
+        urpass = request.GET['user_pass']
+        se_byear = request.GET['byear']
+
+    except:
+        urid = None
+
+    if urid != None and urpass == '12345':
+        verified = True
+    else:
+        verifeid = False
+
     return render(request, 'MyForm/index.html', locals())
